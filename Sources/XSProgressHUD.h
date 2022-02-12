438659b04e8858171b407099d6c4acfe9dbf6957
Xaofly Sho <shaoxiaof@hotmail.com>
2022-02-12 18:21:02 +0800

    Create XSProgressHUD project

---
+++ Sources/XSProgressHUD.h
@@ -0,0 +1,18 @@
+//
+//  XSProgressHUD.h
+//  XSProgressHUD
+//
+//  Created by 邵晓飞 on 2022/2/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XSProgressHUD.
+FOUNDATION_EXPORT double XSProgressHUDVersionNumber;
+
+//! Project version string for XSProgressHUD.
+FOUNDATION_EXPORT const unsigned char XSProgressHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XSProgressHUD/PublicHeader.h>
+
+
---
+++ Sources/XSProgressHUD.swift
@@ -0,0 +1,13 @@
+//
+//  XSProgressHUD.swift
+//  XSProgressHUD
+//
+//  Created by 邵晓飞 on 2022/2/12.
+//
+
+import Foundation
+import UIKit
+
+class XSProgressHUD: UIView {
+    
+}
